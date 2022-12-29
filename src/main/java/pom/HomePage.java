d3dcd6ef1b8dc3a77b00c9d609b26895e2fd4219
Andrey Anisimov <a.a._anisimov@mail.ru>
2022-12-29 19:42:32 +0300

    Add POM structure and pom.xml

---
+++ src/main/java/pom/HomePage.java
@@ -0,0 +1,17 @@
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    private By topButtonOrder = By.className("Button_Button__ra12g"); //верхняя кнопка Заказать
+    private By
+    private By
+    private By
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
